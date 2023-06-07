32e2aef795840faf21caacfd1d68264f59866ad4
Lucas <lucasvonelm@gmail.com>
2023-06-07 13:29:08 -0300

    Funcion ejecutar alarma del ejecutador

--- AdopcionAnimales/src/main/java/Modelo/Alarma/EjecutadorAlarma.java
+++ AdopcionAnimales/src/main/java/Modelo/Alarma/EjecutadorAlarma.java
@@ -2,6 +2,7 @@ package Modelo.Alarma;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class EjecutadorAlarma {
 
@@ -37,5 +38,26 @@ public class EjecutadorAlarma {
 
     public void setUltimaBarrida(Date ultimaBarrida) {
         this.ultimaBarrida = ultimaBarrida;
+    }
+
+    public void ejecutar(){
+
+        Date dateNow = new Date();
+        long diferenciaInMs;
+        long diferenciaInMin;
+        long tiempoActualInMs = dateNow.getTime();
+        long ultimaEjecucionAlarmaInMs;
+
+        for (Alarma alarma : listadoAlarmas){
+
+            ultimaEjecucionAlarmaInMs = alarma.getUltimaEjecucion().getTime();
+            diferenciaInMs = tiempoActualInMs - ultimaEjecucionAlarmaInMs;
+            diferenciaInMin = TimeUnit.MILLISECONDS.toMinutes(diferenciaInMs); //Calculo minutos desde que se disparo
+
+            if (diferenciaInMin >= this.cuantoMin){ //Chequeo si disparo la alarma
+                alarma.dispararAlarma();
+            }
+
+        }
     }
 }
