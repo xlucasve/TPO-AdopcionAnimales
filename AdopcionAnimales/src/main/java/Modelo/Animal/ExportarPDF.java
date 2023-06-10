155f418c482f8355975ba25d9fa55123a4482007
Lucas <lucasvonelm@gmail.com>
2023-06-10 15:11:00 -0300

    Cambiado nombre PDF exportado

--- AdopcionAnimales/src/main/java/Modelo/Animal/ExportarPDF.java
+++ AdopcionAnimales/src/main/java/Modelo/Animal/ExportarPDF.java
@@ -13,15 +13,18 @@ public class ExportarPDF implements EstrategiaExportar {
     public void exportarFicha(FichaTecnica fichaTecnica) {
         try {
             Document documento = new Document();
-            PdfWriter escritor = PdfWriter.getInstance(documento, new FileOutputStream("fichasExportadas/FichaAnimal" + fichaTecnica.getIdFicha() + ".pdf"));
+            PdfWriter escritor = PdfWriter.getInstance(documento, new FileOutputStream("fichasExportadas/FichaAnimal_nro" + fichaTecnica.getIdFicha() + ".pdf"));
             documento.open();
             Paragraph paragrafo = new Paragraph("Ficha exportada a PDF");
             documento.add(paragrafo);
 
-            paragrafo = new Paragraph("\nEsta el animal adoptado? " + fichaTecnica.isAdoptado());
+
+            paragrafo = new Paragraph("\nEstado de adopcion de animal: " + fichaTecnica.isAdoptado());
             documento.add(paragrafo);
 
+
             documento.close();
+            escritor.close();
 
             System.out.println("Se exporto a PDF correctamente");
         } catch (DocumentException e) {
